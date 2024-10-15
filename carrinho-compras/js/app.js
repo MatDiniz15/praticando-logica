f0326cf7de8d750afd21553312ad729c57aa3857
Matheus Diniz <127975435+MatDiniz15@users.noreply.github.com>
2024-10-15 16:07:03 -0300

    Add files via upload

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,24 @@
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor
+
+     let produto = document.getElementById('produto').value;
+     let nomeProduto = produto.split('-')[0];
+     let valorUnitario = produto.split('R$')[1];
+     let quantidade = document.getElementById('quantidade').value;
+
+     //calcular o preço, o nosso subtotal
+     let preco = quantidade * valorUnitario;
+
+     let carrinho = document.getElementById('lista-produtos');
+     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
+
+
+    //adicionar o produto no carrinho
+    //atualizar o valor total da compra
+}
+
+function limpar() {
+    
+}
